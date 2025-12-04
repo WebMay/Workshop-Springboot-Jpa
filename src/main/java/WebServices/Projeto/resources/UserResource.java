225e851ed7e33ac8bcf7d4fc37201432c217d067
Mayara Renata <mayararenataa@gmail.com>
2025-12-03 22:31:14 -0300

    User entity and resource

---
+++ src/main/java/WebServices/Projeto/resources/UserResource.java
@@ -0,0 +1,21 @@
+package WebServices.Projeto.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import WebServices.Projeto.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+	    User u  = new User(1L, "Mayara", "mayara@gmail", "808080", "12345");
+	    return ResponseEntity.ok().body(u);
+		
+	}
+
+}
