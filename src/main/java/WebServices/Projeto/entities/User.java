f8599d21ee7d5b86ca3bc488b2bbaf26c0ed4f8d
Mayara Renata <mayararenataa@gmail.com>
2026-01-05 23:37:45 -0300

    Order, Instant, ISO 8601

--- src/main/java/WebServices/Projeto/entities/User.java
+++ src/main/java/WebServices/Projeto/entities/User.java
@@ -1,12 +1,15 @@
 package WebServices.Projeto.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -22,6 +25,9 @@ import jakarta.persistence.Table;
 	private String email;
 	private String phone;
 	private String password;
+	
+	@OneToMany(mappedBy =  "client")
+	private List<Order> orders = new ArrayList<>();
 	
 	
 	public User() {
@@ -85,7 +91,9 @@ import jakarta.persistence.Table;
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
+	public List<Order> getOrders() {
+		return orders;
+	}
 
 	@Override
 	public int hashCode() {
@@ -105,7 +113,5 @@ import jakarta.persistence.Table;
 		return Objects.equals(id, other.id);
 	}
 
-
-	
 
 }
