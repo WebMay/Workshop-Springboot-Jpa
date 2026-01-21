27ef4ef7db21d24a4e4feb464775efde1bbfd307
Mayara Renata <mayararenataa@gmail.com>
2026-01-21 19:14:12 -0300

    Exception handling - delete

--- src/main/java/WebServices/Projeto/services/UserService.java
+++ src/main/java/WebServices/Projeto/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import WebServices.Projeto.entities.User;
 import WebServices.Projeto.repositories.UserRepository;
+import WebServices.Projeto.services.exceptions.DatabaseException;
 import WebServices.Projeto.services.exceptions.ResourceNotFoundException;
 import jakarta.transaction.Transactional;
 
@@ -31,7 +34,14 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		}catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+		
 	}
 	
 	@Transactional
